0bc2dcd359dc4958a7b5e9659224e5628d122277
Simon Birkholz <birkholz@cubert-gmbh.de>
2026-09-10 00:40:42 +0200

    set ceiling to 4.0.0

--- cuvis_swig.cmake
+++ cuvis_swig.cmake
@@ -1,6 +1,6 @@
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/cuvis.c")
 
-find_package(Cuvis 3.4 REQUIRED)
+find_package(Cuvis 3.4...<4.0 REQUIRED)
 
 set(SWIG_FILES ${CMAKE_CURRENT_LIST_DIR}/src/cuvis_il.i)
 
